e2a3a22bb17c46df0cd7d857518bafbb87d15405
Henry Stern <henry@stern.ca>
2014-02-23 14:45:41 -0400

    moved result push to worker lib

--- dwhois/worker.py
+++ dwhois/worker.py
@@ -1,5 +1,7 @@
 import requests
 import time
+import urllib
+import urlparse
 
 def work_queue(request_url, user=None, password=None,
         sleep_min=1, sleep_max=60):
@@ -21,3 +23,17 @@ def work_queue(request_url, user=None, password=None,
 
             time.sleep(error_sleep)
             error_sleep = min(error_sleep*2, sleep_max)
+
+def push_results(domain, text, upload_base_url, user=None, password=None):
+    upload_url = urlparse.urljoin(upload_base_url, urllib.quote(domain, safe=''))
+
+    try:
+        r = requests.put(upload_url,
+            auth=(user,password),
+            data = text,
+            stream=False,
+            verify=False)
+        r.raise_for_status()
+    except (requests.exceptions.HTTPError,
+            requests.exceptions.ConnectionError), e:
+        print e.message
