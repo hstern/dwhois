d1d6a5a85f04bdfbc4fa77a6cd60ab44bae8b5d7
Henry Stern <henry@stern.ca>
2014-02-23 18:39:46 -0400

    version 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='dwhois',
-        version='0.0',
+        version='0.1',
         description='Distributed WHOIS',
         author='Henry Stern',
         author_email='henry@stern.ca',
