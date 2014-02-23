8c508fdd125d233a09d9cfb17f404a9de0cfcb92
Henry Stern <henry@stern.ca>
2014-02-23 15:07:16 -0400

    Moved sleep to its own object type.

---
+++ dwhois/timing.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+class RateLimiter:
+    def __init__(self, sleep_time=1):
+        self.sleep_time = sleep_time
+
+    def __enter__(self):
+        self._t_start = time.time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        t_end = time.time()
+        t_sleep = self.sleep_time() - (t_end - self._t_start)
+        if t_sleep > 0:
+            # TODO logger
+            print 'Sleeping %f' % t_sleep
+            time.sleep(t_sleep)
+        return True
+
+    def sleep_time(self):
+        return self.sleep_time
+
+class PoissonRateLimiter(RateLimiter):
+    def __init__(self, average_time=1):
+        self.average_time = average_time
+
+    def sleep_time(self):
+        return random.expovariate(1.0/self.average_time)
