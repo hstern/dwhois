46d736cf7ff9e87e5d48f42c8fabbf91a8aac9e5
Henry Stern <henry@stern.ca>
2014-02-23 13:50:44 -0400

    Switched to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='dwhois',
         version='0.0',
         description='Distributed WHOIS Worker',
         author='Henry Stern',
         author_email='henry@stern.ca',
-        packages=['dwhois']
+        packages=find_packages(),
         package_data={'dwhois': ['dwhois/default.conf']},
         scripts=['dwhois-worker'],
         )
