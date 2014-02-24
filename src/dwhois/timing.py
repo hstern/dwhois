7eda9de3841ff19c984ea7313f3ecedcf956f0b4
Henry Stern <henry@stern.ca>
2014-02-24 10:36:21 -0400

    dwhois.timing: Added docs

--- src/dwhois/timing.py
+++ src/dwhois/timing.py
@@ -1,8 +1,33 @@
+"""
+This package is used to rate limit anything.
+
+Use it like this::
+
+    with RateLimiter(sleep_time=1):
+        do your action
+
+It will sleep long enough so that the block takes at least the full
+sleep time.
+
+There are also randomized versions that follow the same use pattern.
+
+Write your own by subclassing RateLimiter and overriding
+RateLimiter.sleep_time.  There is no need to call super.__init__ but you
+may if you want to.  Don't override __enter__ and __exit__.
+"""
+
 import random
 import time
 
 class RateLimiter:
+    """
+    Constant duration rate limiter.
+    """
     def __init__(self, sleep_time=1):
+        """
+        @param sleep_time: How long each iteration should take.
+        @type sleep_time: float seconds
+        """
         self.sleep_time = sleep_time
 
     def __enter__(self):
@@ -10,6 +35,9 @@ class RateLimiter:
         return self
 
     def __exit__(self, type, value, traceback):
+        """
+        Sleeps for sleep_time - elapsed_time.
+        """
         t_end = time.time()
         t_sleep = self.sleep_time() - (t_end - self._t_start)
         if t_sleep > 0:
@@ -17,10 +45,25 @@ class RateLimiter:
         return True
 
     def sleep_time(self):
+        """
+        @return: How long this iteration should take.  Not how long to
+        actually sleep.  That is based on the clock.
+        @rval: float seconds
+        """
         return self.sleep_time
 
 class RandomRateLimiter(RateLimiter):
+    """
+    Uniform random rate limiter.
+    """
     def __init__(self, minval=0, maxval=1):
+        """
+        Sleep time is uniformly distributed over (minval,maxval).
+        @type minval: float seconds
+        @type maxval: float seconds
+
+        @raise ValueError: If minval > maxval.
+        """
         if maxval < minval:
             raise ValueError, 'Minval greater than maxval (%d > %d)' % (minval, maxval)
         self.minval = minval
@@ -30,7 +73,16 @@ class RandomRateLimiter(RateLimiter):
         return self.minval + random.random() * (self.maxval - self.minval)
 
 class GaussianRateLimiter(RateLimiter):
+    """
+    Gaussian random variate rate limiter.
+    """
     def __init__(self, mu=1, sigma=1):
+        """
+        Sleep time is normally distributed about mu by sigma.
+
+        @type mu: float seconds
+        @type sigma: float seconds
+        """
         self.mu = mu
         self.sigma = sigma
 
@@ -38,7 +90,16 @@ class GaussianRateLimiter(RateLimiter):
         return max(0, random.gauss(self.mu, self.sigma))
 
 class PoissonRateLimiter(RateLimiter):
+    """
+    Rate limiter designed to mimic a Poisson process.  This one looks
+    the most like random traffic.
+    """
     def __init__(self, average_time=1):
+        """
+        Sleep time is exponentially distributed about average_time.
+
+        @type average_time: float seconds
+        """
         self.average_time = average_time
 
     def sleep_time(self):
