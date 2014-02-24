a72bb51eaa690ab3160c1f1e2fc8672d85d34019
Henry Stern <henry@stern.ca>
2014-02-24 09:47:22 -0400

    Bumped version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='dwhois',
-        version='0.1',
+        version='0.2',
         description='Distributed WHOIS',
         author='Henry Stern',
         author_email='henry@stern.ca',
