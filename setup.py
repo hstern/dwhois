4ec56fa2e8929e09aa499adf0f12da6888c3c4c6
Henry Stern <henry@stern.ca>
2014-03-21 13:53:37 -0300

    Version 0.11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from setuptools import setup
 
 setup(name='dwhois',
-        version='0.10',
+        version='0.11',
         description='Distributed WHOIS',
         long_description="""
 Client and worker for a database of WHOIS records accessed via HTTP.
